dd4b0e1f4dcf42ab7ef556792a0d95146dd76421
Juan Falconi <juan.falconi@epn.edu.ec>
2024-06-08 00:20:42 -0500

    conexion con render

--- src/database.js
+++ src/database.js
@@ -6,39 +6,39 @@ const { Pool } = pg;
 dotenv.config();
 
 //CONECTARSE DESDE EL RENDER
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   }
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
 
-// pool.on('connect', () => {
-//   console.log('Conectado a la base de datos PostgreSQL');
-// });
+pool.on('connect', () => {
+  console.log('Conectado a la base de datos PostgreSQL');
+});
 
-// pool.on('error', (err) => {
-//   console.error('Error al conectar a la base de datos PostgreSQL', err);
-// });
+pool.on('error', (err) => {
+  console.error('Error al conectar a la base de datos PostgreSQL', err);
+});
 
 //CONECTARSE LOCALMENTE
 
-let pool;
+// let pool;
 
-try {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
+// try {
+//   pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//   });
 
-  pool.on('connect', () => {
-    console.log('Conectado a la base de datos PostgreSQL');
-  });
-} catch (err) {
-  console.error('Error al conectar a la base de datos PostgreSQL', err);
-}
+//   pool.on('connect', () => {
+//     console.log('Conectado a la base de datos PostgreSQL');
+//   });
+// } catch (err) {
+//   console.error('Error al conectar a la base de datos PostgreSQL', err);
+// }
 
 export default pool;
