65f2e2e362572054dcf6576c9f74361aaf0eb00e
Juan Falconi <juan.falconi@epn.edu.ec>
2024-07-07 17:21:13 -0500

    Pruebas 2.0

--- src/database.js
+++ src/database.js
@@ -14,7 +14,7 @@ try {
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: false
+    ssl: true //cambiar esto cuando se suba a la nuve
   });
 
   pool.on('connect', () => {
